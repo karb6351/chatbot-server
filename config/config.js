031eb5013f8912cfbad5cc1f384d92dece72aa80
Ho wai chi <karb6351@gmail.com>
2019-04-29 03:54:04 +0800

    tset

--- config/config.js
+++ config/config.js
@@ -15,11 +15,11 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_DATABASE,
-    host: process.env.PROD_DB_HOST,
-    dialect: process.env.PROD_DB_DIALECT,
+    username: `${process.env.PROD_DB_USERNAME}`,
+    password: `${process.env.PROD_DB_PASSWORD}`,
+    database: `${process.env.PROD_DB_DATABASE}`,
+    host: `${process.env.PROD_DB_HOST}`,
+    dialect: `${process.env.PROD_DB_DIALECT}`,
     port: process.env.PROD_DB_PORT,
     // pool configuration used to pool database connections
     pool: {
@@ -28,6 +28,8 @@ module.exports = {
       idle: 20000,
       acquire: 20000
     },
+    logging: console.log,
+
   }
 }
 
