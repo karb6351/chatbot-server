a26b0d68503ce3939589b8b3ed39c0cd5d657a1d
Ho wai chi <karb6351@gmail.com>
2019-04-29 03:25:10 +0800

    test

--- config/config.js
+++ config/config.js
@@ -24,8 +24,9 @@ module.exports = {
     // pool configuration used to pool database connections
     pool: {
       max: 5,
-      idle: 300000,
-      acquire: 600000,
+      min: 0,
+      idle: 20000,
+      acquire: 20000
     },
   }
 }
