0aafdb2f57ce688554bc154b108b2c304d588160
Ho wai chi <karb6351@gmail.com>
2019-04-30 01:20:13 +0800

    test

--- src/controllers/authentication_controller.js
+++ src/controllers/authentication_controller.js
@@ -1,66 +1,66 @@
 const db = require('../../models');
 
-const passwordHelper = require('../../helpers/password_helper');
+// const passwordHelper = require('../../helpers/password_helper');
 const tokenHelper = require('../../helpers/token_helper');
 const notificationHelper = require('../../helpers/notification_helper');
 
-exports.apiRegister =  (req, res) => {
-	const { username, password } = req.body
-	//do validation
+// exports.apiRegister =  (req, res) => {
+// 	const { username, password } = req.body
+// 	//do validation
 
 	
-	passwordHelper
-		.bcryptPassword(password)
-		.then((hash) => {
-			return db.User.create({
-				username: username,
-				password: hash
-			});
-		})
-		.then((result) => {
-			console.log('register user: ', username);
-			res.status(200).json({
-				status: true,
-				message: 'Successfully to register'
-			});
-		})
-		.catch((error) => {
-			console.error(error.message);
-			res.status(500).json({ success: false, message: 'Internal server error' });
-		});
-};
+// 	passwordHelper
+// 		.bcryptPassword(password)
+// 		.then((hash) => {
+// 			return db.User.create({
+// 				username: username,
+// 				password: hash
+// 			});
+// 		})
+// 		.then((result) => {
+// 			console.log('register user: ', username);
+// 			res.status(200).json({
+// 				status: true,
+// 				message: 'Successfully to register'
+// 			});
+// 		})
+// 		.catch((error) => {
+// 			console.error(error.message);
+// 			res.status(500).json({ success: false, message: 'Internal server error' });
+// 		});
+// };
 
-exports.apiLogin = (req, res) => {
-	const { username, password } = req.body; 
-	//do validation
+// exports.apiLogin = (req, res) => {
+// 	const { username, password } = req.body; 
+// 	//do validation
 
 
-	db.User.findOne({
-		where: { username }
-	})
-		.then((user) => {
-			// throw an exception when user not found
-			if (!user){
-				throw Error('User not found');
-			}
-			// do the password comparsion
-			return passwordHelper.comparePassword(password, user.password);
-		})
-		.then((result) => {
-			//
-			if (result) {
-				// generate jwt when it is authenticated
-				const jwt = tokenHelper.generateToken(username);
-				res.status(200).json({ success: true, token: jwt });
-			} else {
-				throw Error('Invalid password');
-			}
-		})
-		.catch((error) => {
-			console.error(error.message);
-			res.status(404).json({ success: false, message: error.message });
-		});
-};
+// 	db.User.findOne({
+// 		where: { username }
+// 	})
+// 		.then((user) => {
+// 			// throw an exception when user not found
+// 			if (!user){
+// 				throw Error('User not found');
+// 			}
+// 			// do the password comparsion
+// 			return passwordHelper.comparePassword(password, user.password);
+// 		})
+// 		.then((result) => {
+// 			//
+// 			if (result) {
+// 				// generate jwt when it is authenticated
+// 				const jwt = tokenHelper.generateToken(username);
+// 				res.status(200).json({ success: true, token: jwt });
+// 			} else {
+// 				throw Error('Invalid password');
+// 			}
+// 		})
+// 		.catch((error) => {
+// 			console.error(error.message);
+// 			res.status(404).json({ success: false, message: error.message });
+// 		});
+// };
 
 exports.apiLogout = (req, res) => {};
 
@@ -68,69 +68,52 @@ exports.login = (req, res) => {
 	res.render('pages/authentication/login');
 };
 
-exports.doLogin = (req, res) => {
-	const {username, password} = req.body;
-	db.Admin.findOne({
-		where: { username },
-	})
-	.then(user => {
-		if (!user) throw Error('User not found');
-			// do the password comparsion
-			return passwordHelper.comparePassword(password, user.password);	
-	})
-	.then(result => {
-		if (result) {
-			if (req.is('application/json')){
-				const jwt = tokenHelper.generateToken(username);
-				res.status(200).json({
-					token: jwt,
-					success: true
-				})
-			}else{
-				// generate jwt when it is authenticated
-				req.session.username = username
-				req.flash('flash_message', 'asd');
-				// notificationHelper.addFlashMessage(req, notificationHelper.SUCCESS, `Welcome back.${username}`)
-				res.redirect('/home')
-			}
-		} else {
-			throw Error('Invalid password');
-		}
-	})
-	.catch(error => {
-		console.error(error)
-		if (req.is('application/json')){
-			return res.status(200).json({
-				message: error.message,
-				success: false
-			})
-		}else{
-			res.redirect('back')
-		}
-	})
-};
+// exports.doLogin = (req, res) => {
+// 	const {username, password} = req.body;
+// 	db.Admin.findOne({
+// 		where: { username },
+// 	})
+// 	.then(user => {
+// 		if (!user) throw Error('User not found');
+// 			// do the password comparsion
+// 			return passwordHelper.comparePassword(password, user.password);	
+// 	})
+// 	.then(result => {
+// 		if (result) {
+// 			if (req.is('application/json')){
+// 				const jwt = tokenHelper.generateToken(username);
+// 				res.status(200).json({
+// 					token: jwt,
+// 					success: true
+// 				})
+// 			}else{
+// 				// generate jwt when it is authenticated
+// 				req.session.username = username
+// 				req.flash('flash_message', 'asd');
+// 				// notificationHelper.addFlashMessage(req, notificationHelper.SUCCESS, `Welcome back.${username}`)
+// 				res.redirect('/home')
+// 			}
+// 		} else {
+// 			throw Error('Invalid password');
+// 		}
+// 	})
+// 	.catch(error => {
+// 		console.error(error)
+// 		if (req.is('application/json')){
+// 			return res.status(200).json({
+// 				message: error.message,
+// 				success: false
+// 			})
+// 		}else{
+// 			res.redirect('back')
+// 		}
+// 	})
+// };
 
 exports.logout = (req, res) => {
 	req.session.username = null;
 	res.redirect('/home')
 };
-
-exports.doLoginWithPaintPW = (req, res) => {
-	const { username, password } = req.body;
-	db.Admin.findOne({where: {username}})
-	.then(user => {
-		if (!user) throw Error('User not found');
-		if (password == user.password){
-			req.session.username = username
-			req.flash('flash_message', 'asd');
-			// notificationHelper.addFlashMessage(req, notificationHelper.SUCCESS, `Welcome back.${username}`)
-			res.redirect('/home')
-		}else {
-			throw Error('Invalid password');
-		}
-	})
-}
-
 
 exports.doLoginWithPaintPW = (req, res) => {
 	const { username, password } = req.body;
